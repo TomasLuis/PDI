4ad33594905b0f243d0d0e49f84a337027746a48
TomasLuis <joaotomas22@hotmail.com>
2025-04-04 17:52:01 +0100

    Links e Verificação com código enviado para o email

--- registo/urls.py
+++ registo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import register
 
 urlpatterns = [
-    path("registarUtilizador/", register, name="register"),
+    path("registarUtilizador/", register, name="registar"),
 ]
